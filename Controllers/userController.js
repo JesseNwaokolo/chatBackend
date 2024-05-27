8382ef4343c6c490da36666e6c1b54d0552730f7
JesseNwaokolo <109309010+JesseNwaokolo@users.noreply.github.com>
2024-05-27 16:44:10 -0700

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -61,7 +61,7 @@ const findUser = async (req, res)=>{
 const getUsers = async (req, res)=>{
   try{
     const user = await userModel.find()
-    res.status(200).json(user)
+    res.status(200).send(user)
   } catch(err){
     console.log(err)
   }
